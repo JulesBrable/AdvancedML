889c25bf361d7510782255a5cb9ed04ff8081cf0
JulesBrable <jules.brable@inrae.fr>
2024-01-19 16:25:37 +0100

    starting point as input and add page config

--- app/app.py
+++ app/app.py
@@ -3,6 +3,25 @@ import streamlit as st
 from utils.charts import dynamic_plot_hyperparameters
 from utils.config import get_optimizer_choices, get_function_descriptions, get_function_explanations
 
+st.set_page_config(
+        page_title="Advanced ML Project",
+        page_icon="🧠", # this icon appears on the tab of the web page
+        layout="centered",
+        initial_sidebar_state="expanded",
+        menu_items={
+            'Report a bug': "https://github.com/JulesBrable/AdvancedML/issues/new",
+            'About': """ 
+            If you want to read more about the project, you would be interested in going to the corresponding
+            [GitHub](https://github.com/JulesBrable/AdvancedML) repository.
+            
+            Contributions:
+            - [Jules Brablé](https://github.com/JulesBrable) - jules.brable@ensae.fr
+            - [Yedidia Agnimo](https://github.com/Yedson54) - yedidia.agnimo@ensae.fr
+            - [Ayman Limae](https://github.com/Liaym) - ayman.limane@ensae.fr
+            """
+        }
+    )
+
 def setup_sidebar_ui(optimizer_choices):
     optimizer_selected = st.sidebar.selectbox("**Select an Optimizer**", list(optimizer_choices.keys()))
     lr = st.sidebar.slider("**Select the Learning Rate**", 0.0, 0.2, 0.01, 0.001)
@@ -16,7 +35,7 @@ def setup_sidebar_ui(optimizer_choices):
         if optimizer_selected == 'RMSprop':
             hyperparameters['alpha'] = st.sidebar.slider("**Select α (Smoothing Constant)**", 0.0, 0.99, 0.99, 0.01)
 
-    hyperparameters['iterations'] = st.sidebar.slider('**Number of Iterations**', 10, 1000, 30)
+    hyperparameters['iterations'] = st.sidebar.slider('**Number of Iterations**', 10, 1000, 80)
     return hyperparameters
     
 st.title("Optimization Path Visualization with multiple optimization algorithms")
@@ -41,23 +60,28 @@ epsilon = 1.0
 if function_selected == 'Quadratic':
     epsilon = st.sidebar.slider("Select ε for the quadratic function", 0.01, 1.0, 0.1, 0.01)
 
-st.header("Visualizing the Function: ")
+st.markdown(r"(_**Note:** If you wish to reset the default settings, or if the application becomes slow to load, consider reducing the number of iterations or reloading the page._)")
+st.subheader("Visualizing the Function: ")
 st.latex(function_descriptions[function_selected])
 
 explanations = get_function_explanations()
 if function_selected in explanations:
     st.write(explanations[function_selected])
+    
+st.sidebar.header("Starting Point for Optimizer")
+start_x = st.sidebar.slider("X Coordinate", min_value=-4.0, max_value=4.0, value=1.5)
+start_y = st.sidebar.slider("Y Coordinate", min_value=-4.0, max_value=4.0, value=-1.5)
 
-fig_3d, fig_2d = dynamic_plot_hyperparameters(optimizer_name, hyperparameters['lr'], num_iterations, epsilon, function=function_selected, **kwargs)
-
+fig_3d, fig_2d = dynamic_plot_hyperparameters(optimizer_name, hyperparameters['lr'], num_iterations, epsilon, function_selected, initial_guess=[start_x, start_y])
 st.plotly_chart(fig_3d)
 st.plotly_chart(fig_2d)
 
 st.markdown("""
 The first plot is a 3D surface plot of the selected function, showing the landscape over which the optimization algorithm traverses.
 The second plot is a 2D contour plot of the same function, providing another perspective of the optimization path.
-
 The green trace in both plots represents the trajectory of the optimizer as it seeks the function's minimum based on the provided hyperparameters.
 
 Adjust the hyperparameters to see how they affect the optimizer's path and efficiency in finding the function's minimum.
+We recommend that you zoom in on each graph and move around the figure: this can be done by clicking directly on the graph and/or using the tools on the right-hand side of each graph".
 """)
+
--- app/utils/charts.py
+++ app/utils/charts.py
@@ -6,14 +6,17 @@ import numpy as np
 from utils.config import get_function_dict, get_optimizer
 import plotly.graph_objects as go
 
-def dynamic_plot_hyperparameters(optimizer_name, lr, num_iterations, epsilon, function, **kwargs):
+def dynamic_plot_hyperparameters(optimizer_name, lr, num_iterations, epsilon, function, initial_guess=None, **kwargs):
+    # if the function is quadratic, there is an extra parameter (epsilon)
     if function == 'Quadratic':
-        f, f_grad = get_function_dict()['Quadratic'](epsilon)
+        f, f_grad = get_function_dict()['Quadratic'](epsilon) 
     else:
         f, f_grad = get_function_dict()[function]    
     
     
-    initial_guess = torch.tensor([1.5, -1.5], requires_grad=True)
+    if initial_guess is None:
+        initial_guess = [1.5, -1.5]
+    initial_guess_tensor = torch.tensor(initial_guess, requires_grad=True)
 
     x = np.linspace(-4, 4, 600)
     y = np.linspace(-4, 4, 600)
@@ -33,7 +36,7 @@ def dynamic_plot_hyperparameters(optimizer_name, lr, num_iterations, epsilon, fu
     if optimizer_name == 'Adam':
         all_x_k, all_f_k, optimizer_instance = optimize_with_one_optimizer(
             optimizer_cls_name=optimizer_name,
-            x_init=initial_guess.detach().numpy(),
+            x_init=initial_guess_tensor.detach().numpy(),
             loss_fn=lambda x: f(x[0].clone().detach(), x[1].clone().detach()),
             loss_grad=lambda x: f_grad(x[0].clone().detach(), x[1].clone().detach()),
             lr = lr,
@@ -44,7 +47,7 @@ def dynamic_plot_hyperparameters(optimizer_name, lr, num_iterations, epsilon, fu
     else:   
         all_x_k, all_f_k, optimizer_instance = optimize_with_one_optimizer(
             optimizer_cls_name=optimizer_name,
-            x_init=initial_guess.detach().numpy(),
+            x_init=initial_guess_tensor.detach().numpy(),
             loss_fn=lambda x: f(x[0], x[1]),
             lr = lr,
             optim_kwargs=kwargs,
@@ -69,10 +72,43 @@ def dynamic_plot_hyperparameters(optimizer_name, lr, num_iterations, epsilon, fu
     fig_2d = go.Figure(data=[go.Contour(z=Z, x=x, y=y)])
     fig_2d.add_trace(go.Scatter(x=trajectory[:, 0], y=trajectory[:, 1],
                                 mode='lines+markers', name='Optimizer Path',
-                                line=dict(color='green', width=4),
-                                marker=dict(size=4, color='green')))
-    fig_2d.update_layout(title='2D Contour Plot', autosize=False,
-                         width=500, height=500, margin=dict(l=65, r=50, b=65, t=90))
+                                line=dict(color='green', width=1),
+                                marker=dict(size=1, color='green')))
+    for i in range(1, len(trajectory)):
+        fig_2d.add_annotation(
+            x=trajectory[i, 0],   # end point of the arrow (x-coordinate)
+            y=trajectory[i, 1],   # end point of the arrow (y-coordinate)
+            ax=trajectory[i-1, 0],  # start point of the arrow (x-coordinate)
+            ay=trajectory[i-1, 1],  # start point of the arrow (y-coordinate)
+            xref="x",
+            yref="y",
+            axref="x",
+            ayref="y",
+            showarrow=True,
+            arrowhead=2,
+            arrowsize=1,
+            arrowwidth=2,
+            arrowcolor='green'
+        )
+    
+    # to automatically zoom within the plot, we compute the bounds of the trajectory
+    x_min, x_max = np.min(trajectory[:, 0]), np.max(trajectory[:, 0])
+    y_min, y_max = np.min(trajectory[:, 1]), np.max(trajectory[:, 1])
+
+    # and add some padding to the bound for better visibility
+    padding_x = (x_max - x_min) * .2
+    padding_y = (y_max - y_min) * .2
+
+    fig_2d.update_layout(
+        xaxis_range=[x_min - padding_x, x_max + padding_x],
+        yaxis_range=[y_min - padding_y, y_max + padding_y],
+        title='2D Contour Plot',
+        autosize=False,
+        width=500,
+        height=500,
+        margin=dict(l=65, r=50, b=65, t=90)
+    )
+
 
     return fig_3d, fig_2d
                     
